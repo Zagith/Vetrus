a738acf8d01d58949d3c5308accde772b0cb86d1
Zagith <geogrimaldi@icloud.com>
2017-08-23 20:38:58 +0200

    update

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovemen : MonoBehaviour {
+    public float speed = 3.0F;
+    public float rotateSpeed = 5.0F;
+    public float jumpSpeed = 10.0F;
+    public float gravity = 20.0F;
+    public Animator Anim;
+    public bool InAir = false;
+    public bool air=false;
+    private Vector3 moveDirection = Vector3.zero;
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+
+    }
+    void Update()
+    {
+
+
+  
+        CharacterController controller = GetComponent<CharacterController>();
+        Vector3 velocity = GetComponent<CharacterController>().velocity;
+ if(air==false) {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            Anim.SetBool("Run", true);
+        }
+        else
+        {
+            Anim.SetBool("Run", false);
+        }
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+        }
+
+        else if(air==true) {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * 0, 0);
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            Anim.SetBool("Run", false);
+        }
+        else
+        {
+            Anim.SetBool("Run", false);
+        }
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+        }
+
+        if (Input.GetKeyUp("space") && InAir == false  && Anim.GetBool("Run") == false)
+        {     InAir = true;
+            air=true;
+            speed = 0F;
+            Anim.Play("Porco");
+            Anim.SetBool("Jump", true);
+            StartCoroutine(waitSeconds());
+           
+        }
+
+        else if (Input.GetKeyUp("space") && InAir == false  && Anim.GetBool("Run") == true)
+        {    Anim.SetBool("Jumprun", true);
+            Anim.Play("jump_inPlace");
+            InAir = true;
+            Jump();
+            
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        moveDirection.y = jumpSpeed;
+
+        StartCoroutine(waitTwoSeconds());
+        moveDirection.y += gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+        Anim.SetBool("Jump", false);
+        Anim.SetBool("Jumprun", false);
+        InAir = false;
+        air=false;
+    }
+
+
+    IEnumerator waitTwoSeconds()
+    {
+        yield return new WaitForSeconds(1);
+        
+        speed = 10.0F;
+
+    }
+
+
+    IEnumerator waitSeconds()
+    {
+        yield return new WaitForSeconds(0.15F);
+        speed = 5.0F;
+        Jump();
+
+    }
+
+
+
+
+
+}
---
+++ Assets/fhvb.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fhvb : MonoBehaviour {
+ public const float Y_ANGLE_MIN = -10.0f;
+ public const float Y_ANGLE_MAX = 50500.0f;
+
+  //public const float X_ANGLE_MIN = -65.0f;
+ //public const float X_ANGLE_MAX = 65.0f;
+
+ public Transform lookAt;
+ public Transform camTransform;
+
+ private Camera cam;
+
+ public float distance = 10.0f;
+ public float currentX = 0.0f;
+ public float currentY = 0.0f;
+ public float sensivityX = 5.0f;
+ public float sensivityY = 5.0f;
+
+
+ private void Start()
+ {
+ 	camTransform = transform;
+ 	cam = Camera.main;
+
+ }
+
+ private void Update()
+ {  
+ 	currentX += Input.GetAxis("Mouse X");
+ 	currentY += Input.GetAxis("Mouse Y") * (-1);
+   
+
+ 	currentY = Mathf.Clamp(currentY,Y_ANGLE_MIN,Y_ANGLE_MAX);
+ 	//currentX = Mathf.Clamp(currentX,X_ANGLE_MIN,X_ANGLE_MAX);
+ }
+
+
+ private void LateUpdate()
+ {
+ 	Vector3 dir = new Vector3(0,0,-distance);
+ 	Quaternion rotation = Quaternion.Euler(currentY,currentX,0);
+ 	camTransform.position = lookAt.position + rotation * dir;
+ 	camTransform.LookAt(lookAt.position);
+ }
+
+}
